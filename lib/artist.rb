b17f5e301b1f42efc878c1cc1e87d8eaf351e1af
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-08 14:38:45 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,20 +1,46 @@
 class Artist
-  attr_accessor :name, :songs, :all
 
   @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def self.new_by_filename(file)
+    data = file.split("/").last
+    artist = data.split(" - ")[0].strip
+    Artist.find_or_create_by_name(artist)
+  end
+
+  def self.find_or_create_by_name(name)
+    artist = @@all.find{|artist| artist.name == name}
+    !!artist ? artist : Artist.new(name)
+  end
+
+  attr_accessor :name, :songs
+
 
   def initialize(name)
     @name = name
     @songs = []
+    self.save
   end
 
   def add_song(song)
-    @songs << song
+    self.songs << song
   end
+
+  def save
+    @@all << self
+  end
+
+
+  def print_songs
+    self.songs.each{|song| puts song.name}
+  end
+
+
 end
-
-
-
 
 
 
@@ -27,13 +53,13 @@ end
 #     @@all
 #   end
 #
-#   def self.new_by_filename(file)
+#    def self.new_by_filename(file)
 #     data = file.split("/").last
 #     artist = data.split(" - ")[0].strip
 #     Artist.find_or_create_by_name(artist)
 #   end
 #
-#   def self.find_or_create_by_name(name)
+#    def self.find_or_create_by_name(name)
 #     artist = @@all.find{|artist| artist.name == name}
 #     !!artist ? artist : Artist.new(name)
 #   end
@@ -58,54 +84,7 @@ end
 #
 #   def print_songs
 #     self.songs.each{|song| puts song.name}
-#   end
+  # end
 #
 #
 # end
-#
-#
-#
-#
-# # class Artist
-# #
-# #   @@all = []
-# #
-# #   def self.all
-# #     @@all
-# #   end
-# #
-# #    def self.new_by_filename(file)
-# #     data = file.split("/").last
-# #     artist = data.split(" - ")[0].strip
-# #     Artist.find_or_create_by_name(artist)
-# #   end
-# #
-# #    def self.find_or_create_by_name(name)
-# #     artist = @@all.find{|artist| artist.name == name}
-# #     !!artist ? artist : Artist.new(name)
-# #   end
-# #
-# #   attr_accessor :name, :songs
-# #
-# #
-# #   def initialize(name)
-# #     @name = name
-# #     @songs = []
-# #     self.save
-# #   end
-# #
-# #   def add_song(song)
-# #     self.songs << song
-# #   end
-# #
-# #   def save
-# #     @@all << self
-# #   end
-# #
-# #
-# #   def print_songs
-# #     self.songs.each{|song| puts song.name}
-# #   end
-# #
-# #
-# # end
