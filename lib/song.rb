4010aa52f128b37b6b30847ac5a516211d9f8c35
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-08 14:41:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,7 +7,10 @@ class Song
     @name = name
   end
 
-
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
 
   def self.new_by_filename(file_name)
     song = file_name.split(" - ")[1]
@@ -17,8 +20,5 @@ class Song
     new_song
   end
 
-  def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
-    artist.add_song(self)
-  end
+  
 end
