2b5314fcc75fb2352fac274a9c54900cb0587625
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-08 10:26:07 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,12 +6,12 @@ class Artist
     @@all
   end
 
-  #  def self.new_by_filename(file)
-  #   data = file.split("/").last
-  #   artist = data.split(" - ")[0].strip
-  #   Artist.find_or_create_by_name(artist)
-  # end
-  #
+   def self.new_by_filename(file)
+    data = file.split("/").last
+    artist = data.split(" - ")[0].strip
+    Artist.find_or_create_by_name(artist)
+  end
+  
    def self.find_or_create_by_name(name)
     artist = @@all.find{|artist| artist.name == name}
     !!artist ? artist : Artist.new(name)
