bda33fab223fee81f893eba2a96bc817bae2a0d2
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-08 13:47:41 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,11 +1,11 @@
 class MP3Importer
-
   attr_accessor :files, :path
 
   def initialize(path)
     @path = path
     @files = Dir[path+ "/*"].select{|file| file.end_with?(".mp3")}.map{|file_name| file_name.split("/").last}
   end
+
 
   def import
       @files.each do |data|
@@ -16,4 +16,5 @@ class MP3Importer
         artist.add_song(song)
       end
   end
+
 end
