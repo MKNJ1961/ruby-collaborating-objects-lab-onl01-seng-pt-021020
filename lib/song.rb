15829de95ba4d32c10ed2b267e2c01684835e190
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-08 14:45:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,20 +14,20 @@ class Song
 end
 
 
-# 
+#
 # class Song
-# 
+#
 #   attr_accessor :name, :artist
-# 
+#
 #   def initialize(name)
 #     @name = name
 #   end
-# 
+#
 #   def artist_name=(name)
 #     self.artist = Artist.find_or_create_by_name(name)
 #     artist.add_song(self)
 #   end
-# 
+#
 #   def self.new_by_filename(file_name)
 #     song = file_name.split(" - ")[1]
 #     artist = file_name.split(" - ")[0]
@@ -36,6 +36,6 @@ end
 #     new_song
 #     song
 #   end
-# 
-# 
+#
+#
 # end
